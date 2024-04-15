60f68b6f0e2eab9fb7e008e9450a4ac89718bc4d
Mayank4352 <120477383+Mayank4352@users.noreply.github.com>
2024-04-15 13:38:23 +0530

    feat: Initial Commit

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  api: {
+    server: 'http://localhost:8000/',
+    mapKey: 'a1e9793d-bed4-4986-949c-24f3abf9e654',
+    googleAuthClientId: '',
+    webSocketUrl: ''
+  }
+};
